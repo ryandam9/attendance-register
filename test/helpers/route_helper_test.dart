91523d1500a19af566c16675b2610ec1dfa62010
ryandam9 <57579364+ryandam9@users.noreply.github.com>
2026-09-02 15:10:41 +1000

    Reset platform override within navigation tests

--- test/helpers/route_helper_test.dart
+++ test/helpers/route_helper_test.dart
@@ -4,28 +4,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  tearDown(() => debugDefaultTargetPlatformOverride = null);
-
   testWidgets('opens secondary tasks in a dialog on macOS', (tester) async {
     debugDefaultTargetPlatformOverride = TargetPlatform.macOS;
-    await _pumpLauncher(tester);
+    try {
+      await _pumpLauncher(tester);
 
-    await tester.tap(find.text('Open'));
-    await tester.pumpAndSettle();
+      await tester.tap(find.text('Open'));
+      await tester.pumpAndSettle();
 
-    expect(find.byType(Dialog), findsOneWidget);
-    expect(find.text('Secondary task'), findsOneWidget);
+      expect(find.byType(Dialog), findsOneWidget);
+      expect(find.text('Secondary task'), findsOneWidget);
+    } finally {
+      debugDefaultTargetPlatformOverride = null;
+    }
   });
 
   testWidgets('pushes secondary tasks as pages on Android', (tester) async {
     debugDefaultTargetPlatformOverride = TargetPlatform.android;
-    await _pumpLauncher(tester);
+    try {
+      await _pumpLauncher(tester);
 
-    await tester.tap(find.text('Open'));
-    await tester.pumpAndSettle();
+      await tester.tap(find.text('Open'));
+      await tester.pumpAndSettle();
 
-    expect(find.byType(Dialog), findsNothing);
-    expect(find.text('Secondary task'), findsOneWidget);
+      expect(find.byType(Dialog), findsNothing);
+      expect(find.text('Secondary task'), findsOneWidget);
+    } finally {
+      debugDefaultTargetPlatformOverride = null;
+    }
   });
 }
 
